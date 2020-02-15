fbaa20bec613e6fcef78a643c6dc684fee3dc8e7
Chris Vincent <cdvincent89@gmail.com>
2020-02-15 13:44:31 -0500

    randomize function working/updated nav

--- src/App.js
+++ src/App.js
@@ -7,35 +7,57 @@ import choices from "./Choices.js";
 class App extends Component {
 
         state = {
-                choices: choices,
+                choices,
                 chosen: [],
                 score: 0,
                 highScore: 0,
                 message: "Select an image!"
         };
         
-        pushArray = () => {
-                this.setState({ chosen: this.push(this.state.chosen)})
-                console.log(this.state.chosen);
+        randomizeChoices = id => {
+                let chosen = this.state.chosen;
+
+                if (choices.includes(id)) {
+                        this.setState({ choices: [], score: 0, message: "Game over!" });
+                        return;
+                } else {
+                        chosen.push(id);
+                        console.log(chosen);
+                        if (choices.length === 12) {
+                                this.setState({message: "You Win!", chosen: []});
+                        }
+                
+
+                this.setState({ choices, chosen, score: chosen.length, message: "Select an image!" });
+
+                for (let i = choices.length - 1; i > 0; i--) {
+                        let j = Math.floor(Math.random() * (i + 1));
+                        [choices[i], choices[j]] = [choices[j], choices[i]];
+                }
         }
-        shoot() {
-                alert("Great Shot!");
-              }
+}
+        
 
         render() {                
                 return (
+                <div>
+                <Nav 
+                        score={this.state.score}
+                        highScore={this.state.highScore}
+                        message={this.state.message}
+                />
                 <Wrapper>
-                        <Nav />
                         {this.state.choices.map(choice => (
                         <Card 
                                 id={choice.id}
+                                key={choice.id}
                                 name={choice.name} 
                                 image={choice.image}
-                                key={choice.id}
-                                onClick={this.shoot()}
+                                randomizeChoices={this.randomizeChoices}
                         />
                         ))}
                 </Wrapper>
+                </div>
                 );
         };
         
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,10 @@ const Card = props => {
 
     return (
         <div className="card">
-      <div className="img-container">
-        <img style={{width:175, height:120}} alt={props.name} src={props.image} onClick={this.shoot} />
-      </div>
-    </div>
+            <div className="img-container">
+                <img style={{width:175, height:120}} alt={props.name} src={props.image} onClick={() => props.randomizeChoices(props.id)} />
+            </div>
+        </div>
     )
 }
 
